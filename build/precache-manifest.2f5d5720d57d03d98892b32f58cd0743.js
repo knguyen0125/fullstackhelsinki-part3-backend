27e0b8ec0dd71c47b307a882450d37fdd5297b5b
Kien Nguyen <k.nguyen0125@gmail.com>
2019-08-27 23:40:38 +0700

    uibuild

---
+++ build/precache-manifest.2f5d5720d57d03d98892b32f58cd0743.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e5d35ab49781f619657ca6d02be4e727",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0d1dde2829eefd643afa",
+    "url": "/static/css/main.3b6ee1a2.chunk.css"
+  },
+  {
+    "revision": "e531ad2ce5ea044f30ae",
+    "url": "/static/js/2.19635263.chunk.js"
+  },
+  {
+    "revision": "0d1dde2829eefd643afa",
+    "url": "/static/js/main.b445b205.chunk.js"
+  },
+  {
+    "revision": "643b1496a8e20922303c",
+    "url": "/static/js/runtime~main.bf59cc56.js"
+  }
+]);
